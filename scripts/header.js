6f43f82cd769ce003f4734b88e97445c6372454f
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-13 15:20:11 +0330

    #

--- scripts/header.js
+++ scripts/header.js
@@ -6,18 +6,27 @@
 //   });
 // });
 
-// function menuItemsHandler(elements) {
+function menuItemsHandler(elements) {
 
-//     elements.style.display = "block";
+    elements.style.display = "block";
 
-// }
+}
 
-// const searchBox = document.querySelector(".search-box");
-// const searchModal = document.querySelector(".search-modal");
-// searchBox.addEventListener("click", () => {
-//   searchModal.style.display = "block";
-// });
+const searchBox = document.querySelector(".search-box");
+const searchModal = document.querySelector(".search-modal");
+const crossIcon = document.querySelector(".fa-times");
+const downIcon = document.querySelector(".fa-caret-down");
+const carToggleSub = document.querySelector(".car-toggle-submenu")
 
+searchBox.addEventListener("click", () => {
+  searchModal.style.display = "block";
+});
+ crossIcon.addEventListener("click",()=>{
+    searchModal.style.display="none"
+ })
 
-
+downIcon.addEventListener("click",()=>{
+    
+carToggleSub.style.display = carToggleSub.style.display === "block" ? "none": "block"
+})
 
