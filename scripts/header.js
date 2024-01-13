f9eef19282fba900fb8313a266e50764e108e66e
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-13 15:33:51 +0330

    #

--- scripts/header.js
+++ scripts/header.js
@@ -6,27 +6,30 @@
 //   });
 // });
 
-function menuItemsHandler(elements) {
+// function menuItemsHandler(elements) {
 
-    elements.style.display = "block";
+//     elements.style.display = "block";
 
-}
+// }
 
 const searchBox = document.querySelector(".search-box");
 const searchModal = document.querySelector(".search-modal");
 const crossIcon = document.querySelector(".fa-times");
-const downIcon = document.querySelector(".fa-caret-down");
-const carToggleSub = document.querySelector(".car-toggle-submenu")
+const carToggle = document.querySelector(".car-toggle");
+const carToggleSub = document.querySelector(".car-toggle-submenu");
 
 searchBox.addEventListener("click", () => {
   searchModal.style.display = "block";
 });
- crossIcon.addEventListener("click",()=>{
-    searchModal.style.display="none"
- })
+crossIcon.addEventListener("click", () => {
+  searchModal.style.display = "none";
+});
 
-downIcon.addEventListener("click",()=>{
-    
-carToggleSub.style.display = carToggleSub.style.display === "block" ? "none": "block"
-})
+carToggle.addEventListener("click", () => {
+  carToggleSub.style.display =
+    carToggleSub.style.display === "block" ? "none" : "block";
+});
 
+searchBox.addEventListener("click", () => {
+  searchModal.style.display = "block";
+});
