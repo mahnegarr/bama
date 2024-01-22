fff664647b05da056277c6a9b2e42948aa63c7ce
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-22 22:19:14 +0330

    #

--- scripts/header.js
+++ scripts/header.js
@@ -114,24 +114,8 @@ accountElement.addEventListener("mouseout", () => {
   accountSubmenu.style.display = "none";
 });
 
-let minValue = document.getElementById("min-value");
-let maxValue = document.getElementById("max-value");
 
-const rangeFill = document.querySelector(".range-fill");
-function validateRange() {
-  let minPrice = parseInt(inputElements(0).value);
-  let maxPrice = parseInt(inputElements(1).value);
 
-  const minPercentage = (minPrice - 10 / 2999999990) * 100;
-  const maxPercentage = (maxPrice - 10 / 2999999990) * 100;
 
-  rangeFill.style.left = minPercentage+"%";
-  rangeFill.style.width = maxPercentage - minPercentage+"%";
-  minValue.innerHTML = "تومان" + minPrice;
-  maxValue.innerHTML = "تومان" + maxPrice;
-}
-const inputElements = document.querySelectorAll("input");
 
-inputElements.forEach((e) => {
-  e.addEventListener("input", validateRange);
-});
+
